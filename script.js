939f0a50850363db7eee67580700cd853a8180aa
ALEKSANDR IUSKEVICH <107239203+AleksandrIuskevichFromBug@users.noreply.github.com>
2024-05-12 19:15:04 +0000

    bol1

---
+++ script.js
@@ -0,0 +1,29 @@
+//window.alert("helli_gocha");
+//alert("helli_gocha");
+// [2,3,6,56889].forEach(alert);
+// let gor = 8==1;
+// alert(gor);
+// alert (typeof 678n);
+// let _koLy = "1234567890";
+// alert(`khkkjl ${_koLy}`);
+// let result = prompt("how mach is it?");
+// alert(typeof(result)+1+result);
+// let result = confirm ( "are you sure?");
+// let u = 5;
+// u*=99+1;
+// alert("j" + "a" + "v" + "a" + 12);
+"use strict"
+// let a = null, k = 678, lut = 8, b = 78, result;
+// let hurma = (u < k) ? u = 500 : k = 100;
+// alert(hurma);
+// let sorm = (u && k && lut);
+
+// result = (a !== null && a !== undefined) ? a : b;
+
+// alert(a !== null);
+// alert(a !== undefined);
+// alert(false || false);
+// let a, b = "h";
+// // let a = 22;
+// a &&= b;
+// alert (a+b);
